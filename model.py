ff3399f7a8202589866f61aa37ca3b590effd92f
neemiasbsilva <neemiasbsilva@gmail.com>
2020-02-19 12:28:24 -0400

    Apply Deep Convolutional Neural Networks

--- model.py
+++ model.py
@@ -1,20 +1,29 @@
 # build the simplest possible auto enconder
-from keras.layers import Input, Dense
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D
 from keras.models import Model
 
 
-def create_auto_encoder(encoding_dim=32, shape=(784,)):
+def create_auto_encoder(encoding_dim=32, shape=(28, 28, 1)):
     # Create the input placeholder, encoded representation, and lossy reconstruction
 
     input_img = Input(shape=shape)
 
-    encoded = Dense(128, activation='relu')(input_img)
-    encoded = Dense(64, activation='relu')(encoded)
-    encoded = Dense(32, activation='relu')(encoded)
+    x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+    x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    encoded = MaxPooling2D((2, 2), padding='same')(x)
 
-    decoded = Dense(64, activation='relu')(encoded)
-    decoded = Dense(128, activation='relu')(decoded)
-    decoded = Dense(784, activation='sigmoid')(encoded)
+    # at this point the representation is (4, 4, 8) i.e. 128-dimensional
+
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+    x = UpSampling2D((2, 2))(x)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = UpSampling2D((2, 2))(x)
+    x = Conv2D(16, (3, 3), activation='relu')(x)
+    x = UpSampling2D((2, 2))(x)
+    decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
 
     auto_encoder = Model(input_img, decoded)
 
