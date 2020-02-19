dc86097304854931363705f658f49a0a17da5883
neemiasbsilva <neemiasbsilva@gmail.com>
2020-02-19 11:28:31 -0400

    Setting the dataset generator

--- dataset_generator.py
+++ dataset_generator.py
@@ -4,6 +4,6 @@ class datasetGenerator:
 
     def __init__(self):
         # we discarding the labels ( since we're only interested in encoding/decoding the inpup images)
-        (x_train, _), (x_test, _) = mnist.load()
+        (x_train, _), (x_test, _) = mnist.load_data()
         self.x_train = x_train
         self.x_test = x_test
