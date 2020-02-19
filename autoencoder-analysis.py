8b00af34e51a29188d0e554142c78e2449664455
neemiasbsilva <neemiasbsilva@gmail.com>
2020-02-19 11:59:42 -0400

    Visualizing the result of auto encoder

--- autoencoder-analysis.py
+++ autoencoder-analysis.py
@@ -1,7 +1,7 @@
 from keras.models import load_model
 from model import *
 from dataset_generator import datasetGenerator
-
+import matplotlib.pyplot as plt
 model = load_model('model_final.h5')
 
 auto_encoder, input_img, encoded, decoded = create_auto_encoder()
@@ -18,3 +18,19 @@ encoded_imgs = encoder.predict(x_test)
 
 decoded_imgs = decoder.predict(encoded_imgs)
 
+n = 10
+
+plt.figure(figsize=(20, 4))
+
+for i in range(n):
+    # display original
+    ax = plt.subplot(2, n, i+1)
+    plt.imshow(x_test[i].reshape(28, 28), cmap='gray')
+    plt.axis('off')
+
+    # display reconstruction
+    ax = plt.subplot(2, n, i+1)
+    plt.imshow(decoded_imgs[i].reshape(28, 28), cmap='gray')
+    plt.axis('off')
+
+plt.show()
