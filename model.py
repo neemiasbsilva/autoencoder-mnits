cc24a5f365745ee0f1f91325aacfde7639501d9b
neemiasbsilva <neemiasbsilva@gmail.com>
2020-02-19 10:32:17 -0400

    Build the autoencoder

--- model.py
+++ model.py
@@ -1,1 +1,11 @@
 # build the simplest possible auto enconder
+from keras.layers import Input, Dense
+from keras.models import Model
+
+class AutoEncode:
+
+    def __init__(self, encoding_dim=32, shape=(784,)):
+        self.enconding_dim = encoding_dim
+        self.shape = shape
+
+    
