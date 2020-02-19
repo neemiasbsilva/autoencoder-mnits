84225af8bd9a464a8c8778dfc260930f563b5ead
neemiasbsilva <neemiasbsilva@gmail.com>
2020-02-19 12:29:59 -0400

    Updating the paramaters of reshape to cnn model

--- train_auto_encoder.py
+++ train_auto_encoder.py
@@ -30,8 +30,8 @@ x_train, x_test = dataset_generator.x_train, dataset_generator.x_test
 # normalize all values between 0 and 1 and flatten
 x_train = np.float32(x_train) / 255
 x_test = np.float32(x_test) / 255
-x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+x_train = x_train.reshape((len(x_train), x_train.shape[1], x_train.shape[2], 1))
+x_test = x_test.reshape((len(x_test), x_test.shape[1], x_test.shape[2], 1))
 
 # train autoencoder for 50 epochs
 nb_epochs = 50
