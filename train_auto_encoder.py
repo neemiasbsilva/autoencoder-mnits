899a75cdd59848bf304827dfdec278abd72f972c
neemiasbsilva <neemiasbsilva@gmail.com>
2020-02-19 11:27:27 -0400

    Choose another optimizer and setting the loss function

--- train_auto_encoder.py
+++ train_auto_encoder.py
@@ -1,7 +1,6 @@
 from model import create_auto_encoder
 from model import create_encoder
 from model import create_decoder
-from keras.optimizers import RMSprop
 from dataset_generator import datasetGenerator
 import numpy as np
 
@@ -12,7 +11,7 @@ encoder = create_encoder(input_img, encoded)
 
 decoder = create_decoder(auto_encoder)
 
-auto_encoder.compile(optimizer=RMSprop, loss='binary_cross_entropy')
+auto_encoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 
 dataset_generator = datasetGenerator()
 
