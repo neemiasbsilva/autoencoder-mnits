8c73c837f91351929a40dae8f4f69a5d82119970
neemiasbsilva <neemiasbsilva@gmail.com>
2020-02-19 11:10:53 -0400

    Get the dataset of x_train & x_test

--- train_auto_encoder.py
+++ train_auto_encoder.py
@@ -2,6 +2,7 @@ from model import create_auto_encoder
 from model import create_encoder
 from model import create_decoder
 from keras.optimizers import RMSprop
+from dataset_generator import datasetGenerator
 import numpy as np
 
 
@@ -12,6 +13,10 @@ encoder = create_encoder(input_img, encoded)
 decoder = create_decoder(auto_encoder)
 
 auto_encoder.compile(optimizer=RMSprop, loss='binary_cross_entropy')
+
+dataset_generator = datasetGenerator()
+
+x_train, x_test = dataset_generator.x_train, dataset_generator.x_test
 
 
 
