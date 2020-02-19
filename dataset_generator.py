d6f516347fcf195c25d8346177d2c692a934faa9
neemiasbsilva <neemiasbsilva@gmail.com>
2020-02-19 10:54:24 -0400

    Dataset generator

---
+++ dataset_generator.py
@@ -0,0 +1,9 @@
+from keras.datasets import mnist
+
+class datasetGenerator:
+
+    def __init__(self):
+        # we discarding the labels ( since we're only interested in encoding/decoding the inpup images)
+        (x_train, _), (x_test, _) = mnist.load()
+        self.x_train = x_train
+        self.x_test = x_test
