05c9448a621cf38aedc021949022598eef73cc81
neemiasbsilva <neemiasbsilva@gmail.com>
2020-02-19 13:25:58 -0400

    Visualizing the result of auto encoder

--- autoencoder-analysis.py
+++ autoencoder-analysis.py
@@ -11,25 +11,26 @@ auto_encoder, input_img, encoded, decoded = create_auto_encoder()
 dg = datasetGenerator()
 
 x_test = dg.x_test
-x_test = np.float32(x_test) / 255
+x_test = x_test.astype('float32') / 255.
 
 x_test = x_test.reshape((len(x_test), x_test.shape[1], x_test.shape[2], 1))
 
 decoded_imgs = auto_encoder.predict(x_test)
 
 n = 10
-
 plt.figure(figsize=(20, 4))
-
 for i in range(n):
     # display original
     ax = plt.subplot(2, n, i+1)
-    plt.imshow(x_test[i].reshape(28, 28), cmap='gray')
-    plt.axis('off')
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
 
     # display reconstruction
-    ax = plt.subplot(2, n, i + 1 + n)
-    plt.imshow(decoded_imgs[i].reshape(28, 28), cmap='gray')
-    plt.axis('off')
-
+    ax = plt.subplot(2, n, i+1 + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
 plt.show()
