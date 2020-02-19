08a73f4732f741126324abad2e6264c5bdbb110c
neemiasbsilva <neemiasbsilva@gmail.com>
2020-02-19 11:07:22 -0400

    Configure the model to use a per-pixel cross entropy

--- train_auto_encoder.py
+++ train_auto_encoder.py
@@ -1,12 +1,17 @@
 from model import create_auto_encoder
 from model import create_encoder
 from model import create_decoder
+from keras.optimizers import RMSprop
+import numpy as np
 
-auto_encoder, input_img, encoded, decoded = create_encoder()
+
+auto_encoder, input_img, encoded, decoded = create_auto_encoder()
 
 encoder = create_encoder(input_img, encoded)
 
 decoder = create_decoder(auto_encoder)
+
+auto_encoder.compile(optimizer=RMSprop, loss='binary_cross_entropy')
 
 
 
